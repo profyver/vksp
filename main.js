11d34295d55d7b5e97a2a4b7b60c6dc25f53c9c8
profyver <profyver@users.noreply.github.com>
2019-06-04 17:40:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,8 +8,13 @@ if (window.location.host=="www.vksp.tk"){
   window.location.replace("https://vksp.tk/")
 }
 function addAcc(){
-  accUsed = accUsed + 1;
-  $("tr:contains('" + "Аккаунт " + acc+ "')").after('<tr><td><p class="control-label">Аккаунт ' + accUsed + ' (<a href="https://oauth.vk.com/authorize?client_id=2685278&redirect_uri=https://api.vk.com/blank.html&display=page&scope=offline%2Cfriends&response_type=token">получить токен</a>)</p><input type="text" name="token'+ accUsed +'" class="form-control" placeholder="от = до &"></td><td><p class="control-label">Ссылка</p><input type="text" name="url'+accUsed+'" class="form-control" placeholder="vk.com/wall-45745333_34298175"></td></tr>');
+  accUsed ++
+  var accPrev = accUsed - 1;
+  $("tr:contains('" + "Аккаунт " + accPrev + "')").after('<tr><td><p class="control-label">Аккаунт ' + accUsed + ' (<a href="https://oauth.vk.com/authorize?client_id=2685278&redirect_uri=https://api.vk.com/blank.html&display=page&scope=offline%2Cfriends&response_type=token">получить токен</a>)</p><input type="text" name="token'+ accUsed +'" class="form-control" placeholder="от = до &"></td><td><p class="control-label">Ссылка</p><input type="text" name="url'+accUsed+'" class="form-control" placeholder="vk.com/wall-45745333_34298175"></td></tr>');
+}
+function delAcc(){
+  $("tr:contains('" + "Аккаунт " + accUsed + "')").remove();
+  accUsed--;
 }
 function changeDataM() {
   var json = $("#mess").val();
